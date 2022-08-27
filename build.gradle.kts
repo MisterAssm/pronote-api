089f8177439e5561765d62e28c0fd4f2d98b74d4
MisterAssm <assim.zpr@gmail.com>
2022-08-27 23:07:51 +0200

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,85 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.`maven-publish`
+import org.gradle.kotlin.dsl.signing
+import java.util.*
+
 plugins {
+    `maven-publish`
+    signing
     kotlin("multiplatform")
     kotlin("plugin.serialization")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply {
+            load(it)
+        }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+} else {
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    // Configure maven central repository
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    // Configure all publications
+    publications.withType<MavenPublication> {
+
+        // Stub javadoc.jar artifact
+        artifact("kronote")
+
+        // Provide artifacts information requited by Maven Central
+        pom {
+            name.set("Kronote")
+            description.set("Library to easily retrieve information from a Pronote server (Index-Education) for JVM/JS/Native")
+            url.set("https://github.com/MisterAssm/pronote-api")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("MisterAssm")
+                    name.set("Assim ZEMOUCHI")
+                    email.set("assim.zpr@gmail.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/MisterAssm/pronote-api")
+            }
+
+        }
+    }
+}
+
+// Signing artifacts. Signing.* extra properties values will be used
+
+signing {
+    sign(publishing.publications)
 }
 
 group = "fr.misterassm.kronote"
@@ -16,6 +95,12 @@ kotlin {
 
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.freeCompilerArgs = listOf(
+                "-module-name",
+                project.path.replace(":", ""),
+                "-Xopt-in=kotlin.RequiresOptIn",
+                "-Xjsr305=strict"
+            )
         }
 
         testRuns["test"].executionTask.configure {
@@ -43,7 +128,6 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(KotlinX.serialization.json)
-                implementation("io.ktor:ktor-client-okhttp:2.1.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
             }
         }
@@ -52,11 +136,39 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        val jvmMain by getting
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp:2.1.0")
+            }
+        }
         val jvmTest by getting
         val jsMain by getting
         val jsTest by getting
         val nativeMain by getting
         val nativeTest by getting
+    }
+
+    val publicationsFromMainHost = listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
+
+    publishing {
+        publications {
+            matching { it.name in publicationsFromMainHost }.all {
+                val targetPublication = this@all
+                tasks.withType<AbstractPublishToMaven>()
+                    .matching { it.publication == targetPublication }
+                    .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
+            }
+        }
+    }
+}
+
+tasks {
+    val shadowJar = withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+        from(sourceSets["main"].output)
+        archiveFileName.set("${rootProject.name}.${extension}")
+    }
+
+    build {
+        dependsOn(shadowJar)
     }
 }
